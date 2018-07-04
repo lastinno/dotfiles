c9f9628946878ed6c71fce847b62edf0ed66b645
Yukinari Tani <yukinarit84@gmail.com>
2018-07-04 09:53:19 +0900

    Configure auto start up ssh-agent

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,5 +1,4 @@
 set fish_greeting
-set -x PYTHON_CONFIGURE_OPTS --enable-shared
 
 alias la "ls -la"
 alias lt "ls -lrt"
@@ -12,6 +11,37 @@ alias vi "vim"
 alias top "top -oRES"
 
 # Python
-set -x PATH "$HOME/.pyenv/bin" $PATH
-status --is-interactive; and . (pyenv init -|psub)
-status --is-interactive; and . (pyenv virtualenv-init -|psub)
+# set -x PATH "$HOME/.pyenv/bin" $PATH
+# if test -e $HOME/.pyenv
+#   set -x PYTHON_CONFIGURE_OPTS --enable-shared
+#   status --is-interactive; and . (pyenv init -|psub)
+#   status --is-interactive; and . (pyenv virtualenv-init -|psub)
+# end
+
+function _ssh_auth_save
+  set -l _sock $HOME/.ssh/ssh_auth_sock.(hostname)
+  if [ $SSH_AUTH_SOCK ]; and test -L $_sock
+    return 0
+  else if [ $SSH_AUTH_SOCK ]; and not test -L $_sock 
+    ln -sf $SSH_AUTH_SOCK $_sock
+  else
+    rm -f $_sock
+	set -x SSH_AUTH_SOCK
+    return 1
+  end
+end
+
+function _startup_ssh_agent
+  if not _ssh_auth_save
+    # If no agent is running and we have a terminal, run ssh-agent and ssh-add
+    eval (ssh-agent -c)
+    /usr/bin/tty > /dev/null; ssh-add
+    _ssh_auth_save
+	return $status
+  end
+  return 0
+end
+
+if not _startup_ssh_agent
+  echo Failed to startup ssh-agent.
+end
